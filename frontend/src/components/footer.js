4de449a477ecc2cddb4e38e99bb5c0e5da703672
jaswinder135 <61361101+jaswinder135@users.noreply.github.com>
2021-01-23 23:25:53 -0500

    footer

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <Container>
                 <Row>
                     <Col className='text-center py-3'>
-                        Copyright  &copy; ShopMaster
+                        Copyright  &copy; ShopMaster 2021
                     </Col>
                 </Row>
 
--- frontend/src/products.js
+++ frontend/src/products.js
@@ -40,7 +40,7 @@ const products = [
   },
   {
     _id: '4',
-    name: 'Sony Playstation 4 Pro White Version',
+    name: 'Sony Playstation 4  White Version',
     image: '/images/playstation.jpg',
     description:
       'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
