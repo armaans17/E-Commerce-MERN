06de32ff378b5c6505d4db553d472328cfcda8c9
armaan singh <armaanmaani@gmail.com>
2021-01-31 19:03:42 -0500

    Changes

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import brcypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
     name: {
@@ -22,6 +23,10 @@ const userSchema = mongoose.Schema({
 }, {
     timestamps: true
 })
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await brcypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
